6119cd2b7dc2a692755b27840b9d9c3d064b27a3
AlexandreDuzentos <Aduzentos12@gmail.com>
2024-05-20 09:21:48 +0100

    Usando padrão DTO para retornar usuários

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.nelioalves.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nelioalves.workshopmongo.DTO.UserDTO;
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.services.UserService;
 
@@ -29,14 +31,17 @@ public class UserResource {
 	 * no caminho "/users", que fornecerá informações para o cliente
 	 **/
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() throws Exception{
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = userService.findAll();
+		
+		/* Convertendo uma List<User> para uma List<UserDTO>*/
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 
 		/* O método ok instância o ResponseEntity já com o código 
 		 * de resposta http que a resposta ocorreu com sucesso
 		 * 
 		 * O método body conterá o corpo da resposta http.
 		 * */
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(listDto);
 	} 
 }
--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nelioalves.workshopmongo.DTO.UserDTO;
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 
