7bc40203bedcb0936cead19a3217dd1126d32450
AlexandreDuzentos <Aduzentos12@gmail.com>
2024-05-26 10:05:57 +0100

    Projecao dos dados do autor com DTO

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.nelioalves.workshopmongo.DTO.AuthorDTO;
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.PostRepository;
@@ -39,12 +40,36 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
-		/* Salvando novos registros na coleção user no MongoDB */
+		/*
+		 *  Salvando novos registros na coleção user no MongoDB
+		 *  
+		 *  Os users estão sendo salvos antes da associação entre
+		 *  posts e os authors para que ele tenham um id próprio, que
+		 *  é gerado pelo banco de dados e para que esse id seja copiado
+		 *  para o AuthorDTO, caso contrário o id do AuthorDTO seria null.
+		 *  
+		 *  O banco de dados gera automaticamente o id dos objetos
+		 *  a serem salvos e a ferramenta de ORM(jpa) se encarregará
+		 *  de buscar o último registro salvo na coleção
+		 *  correspondente a aquele
+		 *  objeto do JPA(já com o id) para popular o objeto que carregou os dados para serem
+		 *  salvos no banco de dados e é assim que os objeto ficam populados
+		 *  com o dados que foram salvos no banco de dados, incluindo o id que foi
+		 *  gerado automáticamente.
+		 *  
+		 *  Dentro do método saveAll já possui uma lógica para salvar
+		 *  os objeto passados como argumento para ele e também para
+		 *  buscar os objetos que foram salvos para popular os objetos
+		 *  passados como argumento com os dados que foram salvos no
+		 *  banco de dados.
+		 *  
+		 *   */
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));	
+		
+		/* Salvando novos registros na coleção post no MongoDB */
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		
